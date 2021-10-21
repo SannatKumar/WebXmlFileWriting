1e61f1f0d806ab9f86bbd408be5ff0553ebb4478
Raj Kumar <interplaytheheads@gmail.com>
2021-10-21 21:24:07 +0300

    Model created for our application

---
+++ Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Create_Xml.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+        public int Age { get; set; }
+        public string CountryName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gmail { get; set; }
+        public string Langauge { get; set; }
+
+        public string Gender { get; set; }
+
+    }
+}
