77216ae275f5ee7d001077f8783e8f39f0106c36
Raj Kumar <interplaytheheads@gmail.com>
2021-10-21 22:37:28 +0300

    HTTPGet for returning the result

---
+++ Controllers/UserDetailController.cs
@@ -0,0 +1,22 @@
+﻿using Create_Xml.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+
+namespace Create_Xml.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDetailController : ControllerBase
+    {
+        public List<User> userList = new List<User>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAllUser() => userList;
+        
+    }
+}
