046f229992da74bd48c329019d5396e93371a312
Raj Kumar <interplaytheheads@gmail.com>
2021-10-21 23:56:46 +0300

    Model Changes for the list and User

--- Models/User.cs
+++ Models/User.cs
@@ -5,27 +5,32 @@ using System.Threading.Tasks;
 
 namespace Create_Xml.Models
 {
-    public class User
+    public class UserModel
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
+        
 
-        public string LastName { get; set; }
-        public string FullName
+        public class User
         {
-            get
-            {
-                return FirstName + " " + LastName;
-            }
-        }
-        public int Age { get; set; }
-        public string CountryName { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Gmail { get; set; }
-        public string Langauge { get; set; }
+            public int Id { get; set; }
+            public string FirstName { get; set; }
 
-        public string Gender { get; set; }
+            public string LastName { get; set; }
+            public string FullName
+            {
+                get
+                {
+                    return FirstName + " " + LastName;
+                }
+            }
+            public int Age { get; set; }
+            public string CountryName { get; set; }
+            public string Address { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Email { get; set; }
+            public string Language { get; set; }
+
+            public string Gender { get; set; }
+        }
 
     }
     
