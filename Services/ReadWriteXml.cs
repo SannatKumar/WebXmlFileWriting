a35f7f072b638c752369efddb86e4a1548ef9acf
Raj Kumar <interplaytheheads@gmail.com>
2021-10-21 22:38:07 +0300

    Function related to saving the file

---
+++ Services/ReadWriteXml.cs
@@ -0,0 +1,23 @@
+﻿using Create_Xml.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Create_Xml.Services
+{
+    public class ReadWriteXml
+    {
+
+        
+
+        public void SaveUserList(User userList)
+        {
+            System.Xml.Serialization.XmlSerializer mySerializeFile = new System.Xml.Serialization.XmlSerializer(typeof(User));
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//UserDetails.xml";
+            System.IO.FileStream file = System.IO.File.Create(path);
+            mySerializeFile.Serialize(file, userList);
+        }
+
+    }
+}
