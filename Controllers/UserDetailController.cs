cfd4c42d209e3c79fabeceb02e91ad756842d5a9
Raj Kumar <interplaytheheads@gmail.com>
2021-10-21 23:56:07 +0300

    Model Changes for the list and User

--- Controllers/UserDetailController.cs
+++ Controllers/UserDetailController.cs
@@ -9,14 +9,38 @@ using Microsoft.AspNetCore.Cors;
 
 namespace Create_Xml.Controllers
 {
+    [EnableCors]
     [Route("api/[controller]")]
     [ApiController]
     public class UserDetailController : ControllerBase
     {
-        public List<User> userList = new List<User>();
+        public List<UserModel.User> userList = new List<UserModel.User>();
 
         [HttpGet]
-        public ActionResult<IEnumerable<User>> GetAllUser() => userList;
-        
+        public ActionResult<IEnumerable<UserModel.User>> GetAllUser()
+        {
+            return userList;
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<UserModel.User>> PostContentList(UserModel.User ContentList)
+        {
+            userList.Add(new UserModel.User
+            {
+                Id = 1,
+                FirstName = "Raj",
+                LastName = "Tiwari",
+                Age = 32,
+                CountryName = "Nepal",
+                Address = "Finland",
+                PhoneNumber = "0449427068",
+                Email = "rajkumarmailuser@gmailc.om",
+                Language = "Nepali",
+                Gender = "Male",
+            }
+            );
+            return userList;
+
+        }
     }
 }
