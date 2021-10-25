5d11a6cbb8e6d7deb6c3cc0b73efff639a7ddbb2
Raj Kumar <interplaytheheads@gmail.com>
2021-10-25 13:38:42 +0300

    Saving function moved to controller

--- Models/User.cs
+++ Models/User.cs
@@ -7,7 +7,9 @@ namespace Create_Xml.Models
 {
     public class UserModel
     {
-        
+        public List<User> UserList { get; set; }
+
+
 
         public class User
         {
@@ -15,13 +17,13 @@ namespace Create_Xml.Models
             public string FirstName { get; set; }
 
             public string LastName { get; set; }
-            public string FullName
-            {
-                get
-                {
-                    return FirstName + " " + LastName;
-                }
-            }
+            //public string FullName
+            //{
+            //    get
+            //    {
+            //        return FirstName + " " + LastName;
+            //    }
+            //}
             public int Age { get; set; }
             public string CountryName { get; set; }
             public string Address { get; set; }
@@ -31,6 +33,13 @@ namespace Create_Xml.Models
 
             public string Gender { get; set; }
         }
+        //public void SaveUserList(User UserList)
+        //{
+        //    System.Xml.Serialization.XmlSerializer mySerializeFile = new System.Xml.Serialization.XmlSerializer(typeof(User));
+        //    var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//UserDetails.xml";
+        //    System.IO.FileStream file = System.IO.File.Create(path);
+        //    mySerializeFile.Serialize(file, UserList);
+        //}
 
     }
     
